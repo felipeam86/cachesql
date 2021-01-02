407be4b614d873eb233731ec9cdc464e906c686c
Felipe Aguirre Martinez <felipeam86@gmail.com>
2021-01-02 16:07:32 +0200

    Import Database from root of the package

--- sqlcache/__init__.py
+++ sqlcache/__init__.py
@@ -1,4 +1,6 @@
 __version__ = "0.1.0"
 import logging
 
+from .sql import Database
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- tests/test_sqlcache.py
+++ tests/test_sqlcache.py
@@ -1,5 +1,23 @@
-from sqlcache import __version__
+from unittest.mock import patch
+
+from sqlcache import Database, __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_load_results_from_cache(query_string, tmp_path):
+    """Test Database can be imported from sqlcache"""
+
+    with patch("sqlcache.Database._query") as mock_query:
+        db = Database(
+            name="db",
+            uri="sqlite:///file:path/to/database1b?mode=ro&uri=true",
+            cache_store=tmp_path,
+        )
+        assert (
+            mock_query.call_count == 0
+        ), "'Database._query' should not have been called up to this point"
+        df = db.query(query_string=query_string)
+        assert mock_query.call_count == 1, "'Database._query' should have been invoked"
